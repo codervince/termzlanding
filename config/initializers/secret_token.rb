5a62666cb5d14516ed5cb8308670e75ef196b7ac
Coder Vince <publicvince102@gmail.com>
2013-03-06 17:24:36 +0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Termzlanding::Application.config.secret_token = '772d402161d04dd906d7ac0dba663183d3afe9f5e849792d601d8bb03ab50769f8bf9c3bc2b5aaad6753578fdeb726982f596b6c645301a21c8ab3607db361a4'
